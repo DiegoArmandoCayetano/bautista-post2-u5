d81409dc03b25b9bc6910387d04e2a79bb2d0657
DiegoArmandoCayetano <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2026-04-12 18:37:34 -0500

    creación de la estructura del proyecto con sus carpetas

---
+++ src/main/java/com/universidad/patrones/PatronesApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronesApplication.class, args);
+	}
+
+}
